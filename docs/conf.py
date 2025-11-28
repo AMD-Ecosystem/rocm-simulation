6f14752cb4b73010564497a14d5b1c5a7bd7a277
anisanka_amdeng <214179780+anisanka_amdeng@users.noreply.github.com>
2025-11-28 00:08:06 -0500

    css style addition and updating compat table

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,10 @@ html_theme_options = {
     "flavor": "rocm-simulation",
     # Add any additional theme options here
 }
+
+html_static_path = ["sphinx/static/css"]
+html_css_files = ["rocm_custom.css"]
+
 extensions = ["rocm_docs"]
 
 # Table of contents
